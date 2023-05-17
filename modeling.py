81e5d28dfd95c383a381b7aba479c318eb4745ab
Andy Jensen <andrew.j.jensen91@gmail.com>
2023-05-17 16:17:41 -0400

    conclusions and next steps added and other minor fixes

--- modeling.py
+++ modeling.py
@@ -24,6 +24,16 @@ def modeling_df(df):
     train, val, test=ex.tts(df, stratify='rank')
     return train, val, test
 
+def get_baseline(train):
+    '''
+    this will give the baseline for the model to beat
+    '''
+    train['baseline']=0
+    y_train=train['rank']
+    baseline = accuracy_score(y_train, train['baseline'])
+    train.drop(columns='baseline', inplace=True)
+    return print(f'The baseline our model needs to beat is: {round(baseline,4)*100}%')
+
 def models(train, val):
     '''
     this function prints results for models
@@ -94,3 +104,46 @@ def models(train, val):
     results['difference']=results['train_accuracy']-results['validate_accuracy'] 
     results=results.sort_values('difference', ascending=False)
     return results
+
+
+def modeling_viz(train, val, test):
+    '''
+    this function will give the visualization for the models
+    '''
+    x_train=train.drop(columns=['rank'])
+    y_train=train['rank']
+
+    x_val=val.drop(columns=['rank'])
+    y_val=val['rank']
+
+    x_test=test.drop(columns=['rank'])
+    y_test=test['rank']
+
+    rm= RandomForestClassifier(max_depth= 9, min_samples_leaf= 10, random_state=8675309)
+    rm.fit(x_train, y_train)
+    trainacc = rm.score(x_train,y_train)
+    valacc = rm.score(x_val, y_val)
+    testacc=rm.score(x_test, y_test)
+
+    plt.figure(figsize=(7,7))
+    X = ['Random Forest']
+    train['baseline']=0
+    baseline=accuracy_score(y_train, train['baseline'])
+
+    X_axis = np.arange(len(X))
+
+    plt.bar(X_axis[0] - 0.2, trainacc, 0.2, label = 'Train Accuracy', color=['blue'], ec='black')
+    plt.bar(X_axis[0] + 0.0, valacc, 0.2, label = 'Validate Accuracy', color=['green'], ec='black')
+    plt.bar(X_axis[0] + 0.2, testacc, 0.2, label = 'Test Accuracy', color=['rebeccapurple'], ec='black')
+
+
+    plt.axhline(y = baseline, color = 'r', linestyle = '-', label='Baseline Accuracy')
+
+    plt.xticks(X_axis, X)
+    plt.xlabel("Model")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy of Model vs Baseline")
+    plt.ylim(0, 1.2)
+    plt.grid(True, alpha=0.3, linestyle='--', axis='y')
+    plt.legend()
+    plt.show()
